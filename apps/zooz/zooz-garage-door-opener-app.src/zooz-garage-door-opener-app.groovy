234a996bff9b0253341ce0c6a39f54493fd71dbf
Kevin LaFramboise <klaframboise@comcast.net>
2020-08-10 09:03:28 -0400

    Update zooz-garage-door-opener-app.groovy

--- apps/zooz/zooz-garage-door-opener-app.src/zooz-garage-door-opener-app.groovy
+++ apps/zooz/zooz-garage-door-opener-app.src/zooz-garage-door-opener-app.groovy
@@ -1,10 +1,13 @@
 /*
- *  Zooz Garage Door Opener App v1.1	(Apps Code)
+ *  Zooz Garage Door Opener App v1.2	(Apps Code)
  *
  *
- * WARNING: Using a homemade garage door opener can be dangerous so use this code at your own risk. 
+ * WARNING: Using a homemade garage door opener can be dangerous so use this code at your own risk.
  *
  *  Changelog:
+ *
+ *    1.2 (08/10/2020)
+ *      - Added notifications for when door fails to open or close.
  *
  *    1.1 (06/03/2020)
  *      - Added optional virtual lock device that can be used by Alexa to control the garage door using a password.
@@ -17,13 +20,13 @@
  *
  *
  *  Copyright 2020 Zooz
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -62,109 +65,125 @@ preferences {
 
 def pageMain() {
 	dynamicPage(name: "pageMain", title: "", install: true, uninstall: false) {
-		
+
 		section("<big><b>Garage Door Name</b></big>") {
 			paragraph "The Garage Door Name will be displayed in the Garage Door list when you open this app."
 
 			paragraph "The app will create a new Device with the same name that you can use to open and close your garage door from Hubitat."
-			
+
 			label title: "<b>Enter Garage Door Name:</b>", required: true
-			
+
 			paragraph ""
 		}
-		
+
 		section("<big><b>Relay Switch</b></big>") {
 			paragraph "The Relay Switch will be used to activate the garage door."
-			
+
 			input "relaySwitch", "capability.switch",
 				title: "<b>Select Relay Switch:</b>",
 				required: true
-			
+
 			paragraph ""
 		}
-		
+
 		section("<big><b>Open/Close Sensor</b></big>") {
 			paragraph "The Open/Close Sensor will be used to determine the state of the garage door."
-			
+
 			input "contactSensor", "capability.contactSensor",
 				title: "<b>Select Open/Close Sensor:</b>",
 				required: true
-			
+
 			paragraph ""
 		}
-		
+
 		section("<big><b>Relay Switch Auto-Off Timer</b></big>") {
 			paragraph "The Auto-Off Timer will turn the relay off after a few seconds making it a momentary switch."
-			
+
 			paragraph "Leave this setting disabled if you already set the switch type on the ZEN16 to Garage Door Mode."
-			
+
 			input "autoOffDelay", "enum",
 				title: "<b>Select Auto-Off Timer:</b>",
 				required: false,
 				defaultValue: autoOffDelaySetting,
 				options: autoOffDelayOptions
-			
+
 			paragraph ""
 		}
-		
+
 		section("<big><b>Garage Door Operating Duration</b></big>") {
 			paragraph "The Operating Duration should be set to a value greater than or equal to the amount of time it takes for the physical garage door to open/close."
-			
+
 			paragraph "The garage door opener device will stay in the OPENING and CLOSING states during that duration and set to the contact sensor's state afterwards."
-									
+
 			input "operatingDuration", "enum",
 				title: "<b>Select Operating Duration:</b>",
 				required: false,
 				defaultValue: operatingDurationSetting,
 				options: operatingDurationOptions
-			
-			paragraph ""			
+
+			paragraph ""
 		}
-		
+
 		section("<big><b>Garage Door Operating Delay</b></big>") {
 			paragraph "The Operating Delay determines the amount of time it waits after changing the garage door device to OPENING/CLOSING before sending the on command to the Relay Switch."
-			
+
 			paragraph "This feature allows you to use the opening/closing statuses to trigger a siren to turn on before the door starts moving."
-			
+
 			input "operatingDelay", "enum",
 				title: "<b>Select Operating Delay:</b>",
 				required: false,
 				defaultValue: operatingDelaySetting,
 				options: operatingDelayOptions
-			
+
+			paragraph ""
+		}
+
+		section("<big><b>Notifications</b></big>") {
+			paragraph "Send push notification when door fails to open and/or close."
+
+			input "failedOpenDevices", "capability.notification",
+				title: "<b>Send notification to these devices(s) when door fails to Open:</b>",
+				multiple: true,
+				required: false
+
+			input "failedCloseDevices", "capability.notification",
+				title: "<b>Send notification to these devices(s) when door fails to Close:</b>",
+				multiple: true,
+				required: false
+
 			paragraph ""
 		}
 
 		section("<big><b>Virtual Lock</b></big>") {
 			paragraph "Amazon Alexa supports creating a password to control locks so enabling this setting creates a lock device that can be used to control the door and the locked/unlocked status will be synced with the door device's closed/open status."
-						
+
 			input "createLock", "bool",
 				title: "<b>Create Virtual Lock?</b>",
 				required: false,
 				defaultValue: false
-			
+
 			paragraph ""
-		}		
-		
-		section("<big><b>Logging</b></big>") {			
-			input "debugLogging", "bool", 
+		}
+
+		section("<big><b>Logging</b></big>") {
+			input "debugLogging", "bool",
 				title: "<b>Enable debug logging?</b>",
-				defaultValue: true, 
+				defaultValue: true,
 				required: false
 		}
-		
+
 		if (state.installed) {
 			section() {
 				href "pageRemove", title: "Remove Garage Door", description: ""
 			}
-		}		
+		}
 	}
 }
 
 
 def pageRemove() {
 	dynamicPage(name: "pageRemove", title: "", install: false, uninstall: true) {
-		section() {			
+		section() {
 			paragraph "<b>WARNING:</b> You are about to remove this door and the Garage Door Opener device it created.", required: true, state: null
 		}
 	}
@@ -174,11 +193,11 @@ def pageRemove() {
 def uninstalled() {
 	try {
 		childDevices?.each {
-			deleteChildDevice(it.deviceNetworkId)			
+			deleteChildDevice(it.deviceNetworkId)
 		}
 	}
 	catch (ex) {
-		
+
 	}
 }
 
@@ -191,7 +210,7 @@ def installed() {
 	initialize()
 }
 
-def updated() {		
+def updated() {
 	log.warn "updated()..."
 
 	unsubscribe()
@@ -201,10 +220,10 @@ def updated() {
 
 void initialize() {
 	def door = childDoorOpener
-	if (!door) {			
+	if (!door) {
 		runIn(3, createChildGarageDoorOpener)
 	}
-	
+
 	def lock = childLock
 	if (settings?.createLock && !lock) {
 		lock = createChildLock()
@@ -213,19 +232,19 @@ void initialize() {
 	else if (!settings?.createLock && lock) {
 		deleteChildDevice(lock.deviceNetworkId)
 	}
-	
+
 	if (lock) {
 		subscribe(lock, "lock", lockEventHandler)
 	}
-	
+
 	subscribe(settings?.relaySwitch, "switch.on", relaySwitchOnEventHandler)
-	subscribe(settings?.contactSensor, "contact", contactEventHandler)		
+	subscribe(settings?.contactSensor, "contact", contactEventHandler)
 }
 
 def createChildGarageDoorOpener() {
 	def child
 	def name = "${app.label}"
-	logDebug "Creating ${name}"	
+	logDebug "Creating ${name}"
 
 	try {
 		child = addChildDevice(
@@ -250,8 +269,8 @@ def createChildGarageDoorOpener() {
 
 def createChildLock() {
 	def name = "${app.label} Lock"
-	
-	logDebug "Creating ${name}"	
+
+	logDebug "Creating ${name}"
 
 	return addChildDevice(
 		"hubitat",
@@ -273,34 +292,34 @@ def childUninstalled() {
 
 
 void childRefresh(childDNI) {
-	logTrace "childRefresh()..."	
+	logTrace "childRefresh()..."
 	checkDoorStatus()
 }
 
 
-void childOpen(childDNI) {	
+void childOpen(childDNI) {
 	handleDigitalOpenCloseCommand("opening")
 }
 
-void childClose(childDNI) {	
+void childClose(childDNI) {
 	handleDigitalOpenCloseCommand("closing")
 }
 
 void handleDigitalOpenCloseCommand(doorStatus) {
-	logTrace "handleDigitalOpenCloseCommand(${doorStatus})"	
-	
+	logTrace "handleDigitalOpenCloseCommand(${doorStatus})"
+
 	String newContactStatus = (doorStatus == "opening" ? "open" : "closed")
 	String oldDoorStatus = childDoorOpener?.currentValue("door")
 	String oldContactStatus = settings?.contactSensor?.currentValue("contact")
-	
+
 	if ((newContactStatus != oldContactStatus) || (newContactStatus != oldDoorStatus)) {
-		sendDoorEvents(doorStatus)		
-		runIn(operatingDelaySetting, turnOnRelaySwitch)	
+		sendDoorEvents(doorStatus)
+		runIn(operatingDelaySetting, turnOnRelaySwitch)
 	}
 	else {
 		logDebug "The Door is already ${newContactStatus}"
 	}
-	
+
 	runIn((operatingDelaySetting + operatingDurationSetting), checkDoorStatus)
 }
 
@@ -313,20 +332,20 @@ void turnOnRelaySwitch() {
 
 void relaySwitchOnEventHandler(evt) {
 	logDebug "Relay Switch Turned ${evt.value}"
-	
+
 	if (autoOffDelaySetting) {
 		runIn(autoOffDelaySetting, turnOffRelaySwitch)
 	}
-	
+
 	switch (childDoorOpener?.currentValue("door")) {
 		case "open":
 			sendDoorEvents("closing")
 			break
 		case "closed":
-			sendDoorEvents("opening")		
+			sendDoorEvents("opening")
 			break
 	}
-	
+
 	runIn(operatingDurationSetting, checkDoorStatus)
 }
 
@@ -343,14 +362,14 @@ void lockEventHandler(evt) {
 			handleDigitalOpenCloseCommand((evt.value == "locked") ? "closing" : "opening")
 		}
 		state.ignoreNextLockEvent = false
-	}	
+	}
 }
 
 
 void contactEventHandler(evt) {
 	logDebug "Contact sensor changed to ${evt.value}"
 	String doorStatus = childDoorOpener?.currentValue("door")
-	
+
 	if ((evt.value == "open") && (doorStatus == "opening")) {
 		// open usually happens immediately so let the scheduled door status check change it.
 	}
@@ -368,7 +387,7 @@ void contactEventHandler(evt) {
 		}
 		runIn(operatingDurationSetting, checkDoorStatus)
 	}
-	
+
 	// backup check in case there was a timing issue resulting in the status getting stuck as opening or closing.
 	runIn(operatingDurationSetting + 3, checkDoorStatus, [overwrite: false])
 }
@@ -376,35 +395,56 @@ void contactEventHandler(evt) {
 
 void checkDoorStatus() {
 	logTrace "checkDoorStatus()..."
-	
+
 	String contactStatus = settings?.contactSensor?.currentValue("contact")
-	
-	if (childDoorOpener?.currentValue("door") != contactStatus) {		
-		sendDoorEvents(contactStatus)		
-	}	
-	
+
+	if (childDoorOpener?.currentValue("door") != contactStatus) {
+		sendDoorEvents(contactStatus)
+	}
+
 	if (autoOffDelaySetting && (settings?.relaySwitch?.currentValue("switch") == "on")) {
 		// The switch is still on for some reason which will prevent the relay from triggering the door next time so turn it off.
 		logDebug "Turning off Relay Switch (backup)..."
-		settings?.relaySwitch?.off() 
+		settings?.relaySwitch?.off()
 	}
 }
 
 
 void sendDoorEvents(value) {
-	def doorOpener = childDoorOpener		
+	def doorOpener = childDoorOpener
+	String expectedValue = getExpectedDoorValue(doorOpener?.currentValue("door"))
+
 	doorOpener?.parse([name: "door", value: value, displayed: true])
-	
+
 	if (value in ["open", "closed"]) {
 		doorOpener?.parse([name: "contact", value: value, displayed: false])
+
 		sendLockEvent(value)
-	}	
+		
+		if (expectedValue != null && expectedValue != value) {
+			sendFailedNotification(expectedValue)
+		}
+	}
 }
 
-void sendLockEvent(contactValue) {
+String getExpectedDoorValue(String oldValue) {
+	if (oldValue == "opening") {
+		return "open"
+	}
+	else if (oldValue == "closing") {
+		return "closed"
+	}
+	else {
+		return null
+	}
+}
+
+
+void sendLockEvent(String contactValue) {
 	if (settings?.createLock && (contactValue in ["open", "closed"])) {
 		def lock = childLock
 		String lockValue = (contactValue == "open" ? "unlocked" : "locked")
+		
 		if (lockValue != lock.currentLock) {
 			state.ignoreNextLockEvent = true
 			lock?.sendEvent(name: "lock", value: lockValue)
@@ -412,13 +452,25 @@ void sendLockEvent(contactValue) {
 	}
 }
 
+void sendFailedNotification(String expectedValue) {
+	String failedStatus = (expectedValue == "open") ? "Open" : "Close"
+	String msg = "${childDoorOpener?.displayName} Failed to ${failedStatus}"
+	
+	def devices = (expectedValue == "open") ? settings?.failedOpenDevices : settings?.failedCloseDevices
+	
+	if (devices) {
+		logDebug "Sending Notification: ${msg}"
+		devices*.deviceNotification(msg)
+	}
+}
+
 
 def getChildDoorOpener() {
-	return childDevices?.find { it.deviceNetworkId?.endsWith("-door") }	
+	return childDevices?.find { it.deviceNetworkId?.endsWith("-door") }
 }
 
 def getChildLock() {
-	return childDevices?.find { it.deviceNetworkId?.endsWith("-lock") }	
+	return childDevices?.find { it.deviceNetworkId?.endsWith("-lock") }
 }
 
 
